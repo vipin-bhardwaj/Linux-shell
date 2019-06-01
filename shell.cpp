d6c1d4b5b6f6dd6c05e02c2b7f79eaf4cca3252a
vipin-bhardwaj <vipinbhardwaj41@gmail.com>
2019-06-02 04:57:56 +0530

    few updates

--- shell.cpp
+++ shell.cpp
@@ -2,26 +2,38 @@
 #include <unistd.h> 
 #include <vector>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
 int main() {
 	//chdir, getcwd
 	char inputCommand[1100];
-	vector<int> previousCommand;
+	vector<string> previousCommand;
+	char temp[1000];
+	char *curDir = getcwd(temp, 100);
+	cout << curDir << " \n";
+
 	while(1) {
-		char temp[1000];
-		char *curDir = getcwd(temp, 100);
-		cout << curDir << " $";
+		
+		curDir = getcwd(temp, 100);
+		cout << ">>>> ";
+		// cout << curDir << " $";
 		cin.getline(inputCommand, 1099, '\n');
+		if(strlen(inputCommand) > 0) {
+			previousCommand.push_back(string(inputCommand));
+		}
 		if(system(inputCommand)) {
+
 			cout << "command not found.\n";
 		} else {
 			char *cmd =  strtok(inputCommand, " ");
 			if(!strcmp(cmd, "cd")) {
 				char *path = strtok(NULL, " ");
+
 				if(path == NULL) {
 					// take me home;
+					chdir(getenv("HOME"));
 				} else {
 					chdir(path);
 				}
